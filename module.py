1eb68609d5d22b4f8d1a1c29af69e3b8b19091cc
charlie <moshier1@msu.edu>
2021-03-28 19:52:10 -0400

    test

--- module.py
+++ module.py
@@ -2,7 +2,7 @@ import os
 import board
 import digitalio
 import busio
-from adafruit_bus_device.spi_device import SPIDevice
+#from adafruit_bus_device.spi_device import SPIDevice
 
 # test if enviroment variable is set, if not set BLINKA_FT232H=1 before running
 os.environ["BLINKA_FT232H"]
@@ -14,6 +14,5 @@ interrupt.direction = digitalio.Direction.INPUT
 # interrupt.value=True
 
 with busio.SPI(board.SCK, board.MOSI, board.MISO) as spi_bus:
-    cs = digitalio.DigitalInOut(board.D3)
-    device = SPIDevice(spi_bus, cs)
-
+#    cs = digitalio.DigitalInOut(board.D3)
+#    device = SPIDevice(spi_bus, cs)

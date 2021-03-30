3ba7cb9424ea67d8c44ef0ec33197227ee86f4f4
charlie <moshier1@msu.edu>
2021-03-29 22:18:23 -0400

    test

--- module.py
+++ module.py
@@ -59,21 +59,25 @@ with open('coord_module.csv', 'w+', newline='') as f:
         while not spi.try_lock():
              pass
         spi.configure(baudrate=2000000, phase=0, polarity=0)
-        sleep(.001)
         chip_select.value = False
-        sleep(.001)
         result_x_l = bytearray(1)
         result_x_h = bytearray(1)
         result_y_l = bytearray(1)
         result_y_h = bytearray(1)
-        sleep(.001)
-        # print(bytes([0x02 | 0x80, 0x01]))
-        # set res 12000
+
+        # set res 12000cpi, default seems to have x y res different
+        # | 0x80 sets MSB to 1 for write operations
+        # 0x77 denotes full 12000cpi
+        # TODO: move this outside of the loop so it only gets set once
         spi.write(bytes([0x0F | 0x80, 0x77]))
+
+        # set and read motion bit to freeze values for read
         spi.write(bytes([0x02 | 0x80, 0x01]))
         spi.write(bytes([0x02 & 0x7f]))
         spi.readinto(result_x_l)
-        # & 0x7y sets MSB to 0
+
+        # & 0x7y sets MSB to 0 for read operations
+        # read x_l, x_h, y_l, y_h registers
         spi.write(bytes([0x03 & 0x7f]))
         spi.readinto(result_x_l)
         spi.write(bytes([0x04 & 0x7f]))
@@ -82,15 +86,16 @@ with open('coord_module.csv', 'w+', newline='') as f:
         spi.readinto(result_y_l)
         spi.write(bytes([0x06 & 0x7f]))
         spi.readinto(result_y_h)
+
         chip_select.value = True
         spi.unlock()
+
         x=bytearray(2)
         y=bytearray(2)
         x=result_x_h+result_x_l
         y=result_y_h+result_y_l
         x_pos+=int.from_bytes(x,byteorder='little',signed=True)
         y_pos += int.from_bytes(y, byteorder='little', signed=True)
-
 
         writer.writerow(row)
         print(int.from_bytes(x,byteorder='little',signed=True),int.from_bytes(y,byteorder='little',signed=True))
@@ -100,6 +105,7 @@ with open('coord_module.csv', 'w+', newline='') as f:
     # print(x.decode('utf-16le'),y.decode('utf-16le'))
     # print(resultt)
 
+# todo: make live updating graph
 style.use('ggplot')
 
 time,x_displacement,y_displacement = np.loadtxt('coord_module.csv', unpack = True, delimiter = ',')
